3859e0751480af130bc6c4e3b15540b99836d127
Shin-Eun-Ji <ejshin0912@gmail.com>
2024-03-29 08:30:37 +0900

    Title: 스승의 은혜, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240329/스승의 은혜/the-grace-form-teacher.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int[] P = new int[N];
+        int[] S = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            P[i] = Integer.parseInt(st.nextToken());
+            S[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            
+            if( sum < B) {
+                sum += (P[i]+S[i]);
+                cnt = i;
+            } else {
+                sum += (P[i]/2+S[i]);
+                cnt = i;
+                
+                if( sum > B) {
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
